35f5c80a021bef8338a10d180fa675af1dc856e9
xiaduokun <xiaduokun@qq.com>
2026-04-13 17:02:01 +0800

    Remove node_modules from tracking

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from 'globals'
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', '.git/**']
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser
+      }
+    },
+    plugins: {
+      vue: pluginVue
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'vue/no-unused-vars': 'warn',
+      'vue/no-mutating-props': 'error',
+      'vue/valid-template-root': 'error'
+    }
+  }
+]
